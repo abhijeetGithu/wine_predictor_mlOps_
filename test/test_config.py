24435374395229d6734e1e2c0ff4fcc018dbdfa4
abhijeetGithu <abhijeetas8660211@gmail.com>
2023-12-09 09:26:24 +0530

    stage-3 Webapp

--- test/test_config.py
+++
@@ -1,6 +0,0 @@
-def test_get_config():
-    config = get_config()
-    assert config['name'] == 'simple_demo'
-    assert config['version'] == '0.0.1'
-    assert config['author'] == 'Your Name'
-    assert config['author_email'] == ''
